cde6cd9a580d6e8c02403f4b929195ff59dd79e6
Andre <asm13august95@gmail.com>
2023-01-20 10:36:34 -0600

    Initial projects setup

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import download from "./download.png";
+import logo from "./logo.svg";
+import preview from "./preview.png";
+import logo2 from "./logo.png";
+
+export { download, logo, preview, logo2 };
